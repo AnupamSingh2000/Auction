a53fe9cfc01692432a1673eff4ebe22b3fc77434
AnupamSingh2000 <87910847+AnupamSingh2000@users.noreply.github.com>
2021-07-24 23:23:33 +0530

    Add files via upload

---
+++ swings.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.util.ArrayList;
+//package com.company;
+class Listing extends JFrame{
+
+//    public static void main(String[] args) {
+//
+//        new Listing();
+//    }
+
+    Listing(ArrayList<String> al,int left,String a,int teamid) {
+
+        final ArrayList<String> wordlist = new ArrayList<String>(al);
+
+        JList<String> displayList = new JList<>(wordlist.toArray(new String[0]));
+
+        JScrollPane scrollPane = new JScrollPane(displayList);
+        JFrame frame = new JFrame("Team "+a+" -"+teamid+" - Total Money spent - "+(70000-left));
+        frame.getContentPane().add(scrollPane);
+        frame.revalidate();
+        frame.repaint();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 550);
+        frame.setVisible(true);
+    }
+
+
+}
